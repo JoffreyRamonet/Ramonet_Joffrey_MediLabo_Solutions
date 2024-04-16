120b240685d1dbb495680252cf74e3de03c87ed9
MiniGTI <joff.ramo@gmail.com>
2024-04-16 22:16:59 +0200

    microservice_back creation

---
+++ microservice_back/src/test/java/com/medilab/microservice_back/MicroserviceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilab.microservice_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
